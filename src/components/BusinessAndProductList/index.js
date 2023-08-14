585f8bd892ed8b6f0cbe6e1eddd0fc6e2b84e56f
Carlos Quiroz <carloseqv29596@gmail.com>
2023-08-14 12:06:29 -0400

    update version 9.7.1

--- src/components/BusinessAndProductList/index.js
+++ src/components/BusinessAndProductList/index.js
@@ -288,7 +288,7 @@ export const BusinessAndProductList = (props) => {
     setErrorQuantityProducts(!categoryState.products?.length)
     setCategoryState({ ...categoryState })
   }
-  
+
   const getLazyProducts = async ({ page, pageSize = categoryStateDefault.pagination.pageSize }) => {
     const parameters = {
       type: orderState.options?.type ?? 1,
@@ -801,7 +801,7 @@ export const BusinessAndProductList = (props) => {
             }
           }
           return product
-        }) 
+        })
         setCategoryState({ ...categoryState, products: updatedProducts })
         showToast(ToastType.Success, result?.enabled
           ? t('ENABLED_PRODUCT', 'Enabled product')
@@ -813,7 +813,7 @@ export const BusinessAndProductList = (props) => {
       showToast(ToastType.Error, err.message)
     }
   }
-  
+
 
   const updateStoreCategory = async (categoryId, updateParams = {}) => {
     try {
@@ -865,13 +865,13 @@ export const BusinessAndProductList = (props) => {
     if (!orderState.loading && orderOptions && !languageState.loading && !props.avoidBusinessLoading) {
       getBusiness()
     }
-  }, [orderOptions, languageState.loading, slug, filterByMenus, professionalSelected])
+  }, [JSON.stringify(orderOptions), languageState.loading, slug, filterByMenus, professionalSelected])
 
   useEffect(() => {
     if (!orderState.loading && orderOptions && !languageState.loading && !businessState.loading && props.avoidBusinessLoading) {
       getBusiness()
     }
-  }, [orderOptions, languageState.loading, slug, filterByMenus, professionalSelected])
+  }, [JSON.stringify(orderOptions), languageState.loading, slug, filterByMenus, professionalSelected])
 
   /**
    * getBusiness if orderState is loading the first time when is rendered
