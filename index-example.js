d1619d2e895d8fc4e7c945717f4bc821691cabe8
Carlos Quiroz <carlos.q@ordering.co>
2024-04-12 11:37:04 -0400

    update version 13.0.46

--- index-example.js
+++ index-example.js
@@ -1,7 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import * as Sentry from '@sentry/react'
-import { Integrations } from '@sentry/tracing'
 import App from './example/App'
 import { OrderingProvider } from './src/contexts/OrderingContext'
 
@@ -17,18 +15,6 @@ const configFile = {
     url: 'https://socket-v3.ordering.co'
   }
 }
-
-Sentry.init({
-  environment: process.env.NODE_ENV,
-  dsn: "https://752122325a48413ebc00c89d06b5bb1c@o460529.ingest.sentry.io/6302875",
-  release: 'ordering-components-release@' + process.env.npm_package_version,
-  integrations: [
-    new Integrations.BrowserTracing()
-  ],
-  // Release health
-  autoSessionTracking: true,
-  tracesSampleRate: 0.2
-})
 
 const wrapper = document.getElementById('app')
 ReactDOM.render(
